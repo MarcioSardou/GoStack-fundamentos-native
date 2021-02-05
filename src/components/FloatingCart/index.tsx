53bdb1e63dc8aeb34ea2bd153776151e6a597d23
Marcio Sardou <marciosut1996@gmail.com>
2021-02-05 15:05:39 -0300

    finalizando tarefas e validando testes

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -15,24 +15,29 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productsSubTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return acc + productsSubTotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productsQuantity = product.quantity;
 
-    return 0;
+      return acc + productsQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,14 +35,14 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      api.get('/products').then(response => setProducts(response.data));
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
